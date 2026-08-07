5c3ab5c9e25588ffde1de44868ee0047dac0b65a
kanishgupta2169-netizen <kanishgupta2169@gmail.com>
2026-08-07 19:42:08 +0530

    varible and datatypes

---
+++ BASIC_01/datatypes.js
@@ -0,0 +1,19 @@
+let name="kanish"
+let age=21
+let isloggedin=true
+
+//datatypes
+//number=> 2 to power 53
+//bigint=> 2 to power 53
+//string=> double quotes ya single quotes
+//boolean=> true/false
+//null=> standalone value
+//undefined=> value is not defined
+//sybol=>unique
+//object
+
+console.table(typeof age);
+console.table(typeof name);
+console.table(typeof isloggedin);
+console.table(typeof undefined);
+console.table(typeof null);
---
+++ BASIC_01/variables.js
@@ -0,0 +1,17 @@
+const accountId=234
+let accountEmail="kanishgupta2169@gmail.accountEmail"
+var accountPassword="kanish12"
+accountcity="bengluru"
+let accountState;
+
+//changing value of variable
+// accountId=678 cannot change
+
+accountEmail="kanish12@gmail.com"
+accountPassword="4355"
+
+console.log(accountId);
+console.table({accountEmail,accountId,accountPassword,accountcity,accountState})
+
+
+//note: we can change value of let and var but we cannot change value of const 
