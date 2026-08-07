052580e3491a3490b1f46ff7d509f6faaeddac17
kanishgupta2169-netizen <kanishgupta2169@gmail.com>
2026-08-07 19:55:54 +0530

    conversion of datatype

---
+++ BASIC_01/data_conversion.js
@@ -0,0 +1,23 @@
+let score ="33"
+console.log(typeof score);
+
+// conversion
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber); 
+
+//if value of score
+// "33"=> 33
+// "33abc"=> NaN
+// "true"=> 1
+
+
+let isloggedin=0;
+console.log(typeof isloggedin);
+
+let booleanisloogedin=Boolean(isloggedin)
+console.log(booleanisloogedin);
+
+//if value of isloggedin
+//1=>true ; 0=>false
+//""=>false
+// "hitesh"=>true
