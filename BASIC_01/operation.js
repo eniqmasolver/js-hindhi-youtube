bf2339191d745902012d1271dd17d87e8bb90e8c
kanishgupta2169-netizen <kanishgupta2169@gmail.com>
2026-08-07 20:11:38 +0530

    operation

---
+++ BASIC_01/operation.js
@@ -0,0 +1,16 @@
+let value= 3
+let negvalue= -value
+console.log(negvalue);
+
+
+
+console.log(3+2);
+console.log(3/2);
+console.log(3**2);
+console.log(3*2);
+console.log(3%2);
+console.log(3-2);
+
+let str1="kanish"
+let str2="gupta"
+console.log(str1+str2);
