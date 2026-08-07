cf54a328459d120307c01394ec261000368e3411
kanishgupta2169-netizen <kanishgupta2169@gmail.com>
2026-08-07 20:23:50 +0530

    comparsion

---
+++ BASIC_01/comparision.js
@@ -0,0 +1,23 @@
+//  console.log(1>=2);
+//  console.log(1>2);
+//  console.log(1<2);
+//  console.log(1<=2);
+
+// console.log(null>0);//false
+// console.log(null>=0);//true
+// console.log(null==0);//false
+// console.log(null<=0);//true
+// console.log(null<0);//false
+
+
+console.log(undefined==0);//false
+console.log(undefined>0);//false
+console.log(undefined<0);//false
+
+//note:=== (it compare datatype and value both)
+//== (it only check value)
+
+console.log("2"===2);//false
+console.log("2"==2);//true
+
+
