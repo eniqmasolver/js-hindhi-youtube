d416ba438891ef60508232fd7155fbf17c33a95d
kanishgupta2169-netizen <kanishgupta2169@gmail.com>
2026-08-23 20:56:01 +0530

    math and number datatype

---
+++ BASIC_01/nums_and_maths.js
@@ -0,0 +1,40 @@
+// const score=200;
+// console.log(score)
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(typeof balance);//object
+// console.log(balance.toFixed(4));
+
+// const othernum=123.78
+// console.log(othernum.toPrecision(3));
+
+// const hundraed=100000
+// console.log(hundraed.toLocaleString('en-IN'));
+
+//-----------MATHS----------------
+
+
+// console.log(Math);
+// console.log(Math.abs(-6));//positive ma convert ker deta haa
+// console.log(Math.round(4.8));//normal round off as you do
+// console.log(Math.ceil(4.1));//it round off to upper value
+// console.log(Math.floor(4.9));//it is round of to lower value
+// console.log(Math.min(1,3,8,2));//minimum value
+// console.log(Math.max(8,2));//max value
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+
+
+
+
+
+
+
